16d77c7894175161b84ef358468fb074802b314d
MaryTennyson <ebrar-atabay@hotmail.com>
2024-04-23 19:55:50 +0300

    third commit

--- shortestpath.py
+++ shortestpath.py
@@ -1,4 +1,5 @@
 import sys
+from timeit import default_timer as timer
 
 class Graph(object):
     def __init__(self, nodes, init_graph):
@@ -63,28 +64,45 @@ def bellman_ford(graph, start_node, target_node):
 
 
 # The algorithm in action!
-nodes = ["Reykjavik", "Oslo", "Moscow", "London", "Rome", "Berlin", "Belgrade", "Athens"]
+nodes = ["Reykjavik", "Oslo", "Moscow", "London", "Rome", "Berlin", "Belgrade", "Athens", "Paris", "Madrid",
+         "Lisbon", "Dublin", "Brussels", "Vienna", "Stockholm", "Warsaw", "Budapest", "Prague", "Zurich", "Copenhagen"]
+
 init_graph = {
-    "Reykjavik": {"Oslo": 5, "London": 4},
-    "Oslo": {"Reykjavik": 5, "Berlin": 1, "Moscow": 3},
-    "Moscow": {"Oslo": 3, "Belgrade": 5, "Athens": 4},
-    "London": {"Reykjavik": 4},
-    "Rome": {"Berlin": 2, "Athens": 2},
-    "Berlin": {"Rome": 2, "Oslo": 1},
-    "Belgrade": {"Moscow": 5, "Athens": 1},
-    "Athens": {"Rome": 2, "Moscow": 4, "Belgrade": 1}
+    "Reykjavik": {"Oslo": 5, "London": 4, "Berlin": 8},
+    "Oslo": {"Reykjavik": 5, "Moscow": 7, "Stockholm": 2},
+    "Moscow": {"Oslo": 7, "Berlin": 6, "Vienna": 10},
+    "London": {"Reykjavik": 4, "Paris": 3, "Madrid": 7},
+    "Rome": {"Berlin": 3, "Athens": 5, "Paris": 8},
+    "Berlin": {"Reykjavik": 8, "Moscow": 6, "Rome": 3},
+    "Belgrade": {"Athens": 3, "Warsaw": 4, "Budapest": 2},
+    "Athens": {"Rome": 5, "Belgrade": 3, "Madrid": 9},
+    "Paris": {"London": 3, "Rome": 8, "Madrid": 5},
+    "Madrid": {"London": 7, "Athens": 9, "Paris": 5},
+    "Lisbon": {"Dublin": 6, "Paris": 10, "Madrid": 11},
+    "Dublin": {"Lisbon": 6, "London": 9, "Paris": 8},
+    "Brussels": {"London": 4, "Paris": 5, "Berlin": 7},
+    "Vienna": {"Berlin": 4, "Prague": 3, "Warsaw": 6},
+    "Stockholm": {"Oslo": 2,"Copenhagen": 5},
+    "Warsaw": {"Vienna": 6, "Berlin": 5, "Budapest": 4},
+    "Budapest": {"Belgrade": 2, "Warsaw": 4, "Prague": 3},
+    "Prague": {"Vienna": 3, "Budapest": 3, "Zurich": 7},
+    "Zurich": {"Prague": 7, "Vienna": 8, "Paris": 10},
+    "Copenhagen": {"Stockholm": 5, "Berlin": 5}
 }
+
 
 graph = Graph(nodes, init_graph)
 
+t0=timer()
+path, distance = bellman_ford(graph, "Reykjavik", "Vienna")
+t1=timer()
 
-path, distance = bellman_ford(graph, "Reykjavik", "Rome")
-print("BF /Shortest path from Reykjavik to Rome with a distance of {}: {}".format(distance, " -> ".join(path)))
+print(t1-t0)
 
+print("BF /Shortest path from Reykjavik to Copenhagen with a distance of {}: {}".format(distance, " -> ".join(path)))
+t0=timer()
 # Find the shortest path from Reykjavik to Oslo
-path, distance = bellman_ford(graph, "Reykjavik", "Moscow")
-print("BF /Shortest path from Reykjavik to Oslo with a distance of {}: {}".format(distance, " -> ".join(path)))
-
-# Find the shortest path from Reykjavik to Athens
-path, distance = bellman_ford(graph, "Reykjavik", "Athens")
-print("BF /Shortest path from Reykjavik to Athens with a distance of {}: {}".format(distance, " -> ".join(path)))
+path, distance = bellman_ford(graph, "Reykjavik", "Stockholm")
+t1=timer()
+print(t1-t0)
+print("BF /Shortest path from Reykjavik to Stockholm with a distance of {}: {}".format(distance, " -> ".join(path)))
