f0f13d1b0951bddfca9120f5fc9ca1e77fdfe06f
MaryTennyson <ebrar-atabay@hotmail.com>
2024-04-23 16:40:14 +0300

    first commit

--- shortestpath.py
+++ shortestpath.py
@@ -1,90 +1,59 @@
-import sys
-
-class Graph(object):
-    def __init__(self, nodes, init_graph):
+class Graph:
+    def __init__(self, nodes, edges):
         self.nodes = nodes
-        self.graph = self.construct_graph(nodes, init_graph)
-     
-        
-    def construct_graph(self, nodes, init_graph):
-        graph = {}
-        for node in nodes:
-            graph[node] = {}
+        self.edges = edges
 
-        graph.update(init_graph) 
+def floyd_warshall(graph, start_node, target_node):
+    n = len(graph.nodes)
+    dist = [[float('inf')] * n for _ in range(n)]
+    pred = [[None] * n for _ in range(n)]
 
-        for node, edges in graph.items():
-            for adjacent_node, value in edges.items():
-                if graph[adjacent_node].get(node, False) == False:
-                    graph[adjacent_node][node] = value
-        
-        return graph
-    
-    def get_nodes(self):
-        return self.nodes
+    for u, v, w in graph.edges:
+        dist[u][v] = w
+        pred[u][v] = u
 
-    def get_outgoing_edges_for_bf(self, node):
-        return [(adjacent_node, value) for adjacent_node, value in self.graph.get(node, {}).items()]   
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    pred[i][j] = pred[k][j]
 
-    def value(self, node1, node2):
-        return self.graph[node1][node2]
-
-
-
-def bellman_ford(graph, start_node, target_node):
-    # Initialization
-    distance = {node: float('inf') for node in graph.nodes}
-    predecessor = {node: None for node in graph.nodes}
-    distance[start_node] = 0
-
-    # Relax edges repeatedly
-    for _ in range(len(graph.nodes) - 1):
-        for u in graph.nodes:
-            for v, w in graph.get_outgoing_edges_for_bf(u):
-                if distance[u] + w < distance[v]:
-                    distance[v] = distance[u] + w
-                    predecessor[v] = u
-
-    # Check for negative cycles
-    for u in graph.nodes:
-        for v, w in graph.get_outgoing_edges_for_bf(u):
-            if distance[u] + w < distance[v]:
-                raise ValueError("Graph contains negative weight cycle")
-
-    # Reconstruct shortest path
     path = []
-    node = target_node
-    while node is not None:
-        path.append(node)
-        node = predecessor[node]
-
-    path.reverse()
-    return path, distance[target_node]
-
+    u = start_node
+    while u != target_node:
+        if pred[u][target_node] is None:
+            return None, float('inf')  # There is no path
+        path.append(u)
+        u = pred[u][target_node]
+    path.append(target_node)
+    return path, dist[start_node][target_node]
 
 # The algorithm in action!
 nodes = ["Reykjavik", "Oslo", "Moscow", "London", "Rome", "Berlin", "Belgrade", "Athens"]
-init_graph = {
-    "Reykjavik": {"Oslo": 5, "London": 4},
-    "Oslo": {"Reykjavik": 5, "Berlin": 1, "Moscow": 3},
-    "Moscow": {"Oslo": 3, "Belgrade": 5, "Athens": 4},
-    "London": {"Reykjavik": 4},
-    "Rome": {"Berlin": 2, "Athens": 2},
-    "Berlin": {"Rome": 2, "Oslo": 1},
-    "Belgrade": {"Moscow": 5, "Athens": 1},
-    "Athens": {"Rome": 2, "Moscow": 4, "Belgrade": 1}
-}
+edges = [
+    ("Reykjavik", "Oslo", 5), ("Reykjavik", "London", 4),
+    ("Oslo", "Reykjavik", 5), ("Oslo", "Berlin", 1), ("Oslo", "Moscow", 3),
+    ("Moscow", "Oslo", 3), ("Moscow", "Belgrade", 5), ("Moscow", "Athens", 4),
+    ("London", "Reykjavik", 4),
+    ("Rome", "Berlin", 2), ("Rome", "Athens", 2),
+    ("Berlin", "Rome", 2), ("Berlin", "Oslo", 1),
+    ("Belgrade", "Moscow", 5), ("Belgrade", "Athens", 1),
+    ("Athens", "Rome", 2), ("Athens", "Moscow", 4), ("Athens", "Belgrade", 1)
+]
 
-graph = Graph(nodes, init_graph)
+graph = Graph(nodes, edges)
 
-
-path, distance = bellman_ford(graph, "Reykjavik", "Rome")
-print("BF /Shortest path from Reykjavik to Rome with a distance of {}: {}".format(distance, " -> ".join(path)))
-
-# Find the shortest path from Reykjavik to Oslo
-path, distance = bellman_ford(graph, "Reykjavik", "Moscow")
-print("BF /Shortest path from Reykjavik to Oslo with a distance of {}: {}".format(distance, " -> ".join(path)))
+# Find the shortest path from Reykjavik to Moscow
+path, distance = floyd_warshall(graph, "Reykjavik", "Moscow")
+if path:
+    print("Shortest path from Reykjavik to Moscow with a distance of {}: {}".format(distance, " -> ".join(path)))
+else:
+    print("There is no path from Reykjavik to Moscow.")
 
 # Find the shortest path from Reykjavik to Athens
-path, distance = bellman_ford(graph, "Reykjavik", "Athens")
-print("BF /Shortest path from Reykjavik to Athens with a distance of {}: {}".format(distance, " -> ".join(path)))
+path, distance = floyd_warshall(graph, "Reykjavik", "Athens")
+if path:
+    print("Shortest path from Reykjavik to Athens with a distance of {}: {}".format(distance, " -> ".join(path)))
+else:
+    print("There is no path from Reykjavik to Athens.")
